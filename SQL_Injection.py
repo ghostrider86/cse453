263f84a05623cfca5ab2251b9af1c61f79e937c1
Nathan Robison <ncrobiso@yahoo.com>
2022-05-27 13:49:50 -0600

    Add make_query

--- SQL_Injection.py
+++ SQL_Injection.py
@@ -1,1 +1,8 @@
+# Function to generate SQL query
+def make_query(username, password):
+    query = "SELECT * FROM users \nWHERE Username=" + "'" + username + "' AND Password=" + "'" + password + "';"
+    print(query)
+    return query
 
+# Main
+make_query("name", "wordpass")
